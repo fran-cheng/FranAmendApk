34ca8d9655ec9ad3a0e94012c26609ff5d013042
fran <chengliangming@hnguangyi.cn>
2022-03-30 18:25:10 +0800

    第一次提交

---
+++ src/main/java/com/amend/apk/Main.java
@@ -0,0 +1,33 @@
+package com.amend.apk;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 程良明
+ * @date 2022/3/30
+ * * 说明:
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    private void changeSmaliWithPublic() {
+
+    }
+
+    private void parsePublicXml() {
+        Map<String, String> typeNameMap = new HashMap<>();
+        Map<String, String> nameIdMap = new HashMap<>();
+
+    }
+
+    /**
+     * key 是原始的id值
+     * value 是type_name
+     */
+    private void parseRSmaliFile() {
+        Map<String, String> idMap = new HashMap<>();
+    }
+}
