46916e58f30cf00f5f32187c707b3c40795d97a3
fran <chengliangming@hnguangyi.cn>
2022-04-02 11:17:36 +0800

    添加命令参数

--- src/main/java/com/amend/apk/Main.java
+++ src/main/java/com/amend/apk/Main.java
@@ -2,6 +2,9 @@ package com.amend.apk;
 
 import com.amend.utils.FileUtils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author 程良明
  * @date 2022/3/30
@@ -11,12 +14,43 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String workPath = "G:\\Java\\public\\app";
-        String packageName = "heiqi.demo";
-        boolean saveFiles = true;
-        boolean changeOtherFiles = true;
 
-        FileUtils fileUtils = new FileUtils(workPath, null, saveFiles, changeOtherFiles);
+        String workPath = null;
+        String packageName = null;
+        boolean saveFiles = false;
+        boolean changeOtherFiles = false;
+        //         -w  workPath
+//         -p  packageName
+//         -s  saveFiles
+//         -c  saveFiles
+
+
+        String lastKey = "";
+        Map<String, String> stringStringMap = new HashMap<>();
+        for (String s : args) {
+            switch (lastKey) {
+                case "-w":
+                    workPath = s;
+                    break;
+                case "-p":
+                    packageName = s;
+                    break;
+                case "-s":
+                    saveFiles = true;
+                    break;
+                case "-c":
+                    changeOtherFiles = true;
+                    break;
+            }
+            lastKey = s;
+        }
+
+        if (workPath == null) {
+            throw new RuntimeException("参数-w 是必须的");
+        }
+
+
+        FileUtils fileUtils = new FileUtils(workPath, packageName, saveFiles, changeOtherFiles);
         fileUtils.execute();
 
 
